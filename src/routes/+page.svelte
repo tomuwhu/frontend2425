0d4944967b9ec372bc727cb3421b44b740aaf2e2
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-09-29 10:44:16 +0200

    .

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Highlight, { LineNumbers } from "svelte-highlight";
+    import javascript from "svelte-highlight/languages/javascript";
+    import atomOneDark from "svelte-highlight/styles/atom-one-dark";
+    const code = "const add = (a: number, b: number) => a + b;";
+</script>
+
+<svelte:head>
+    {@html atomOneDark}
+</svelte:head>
+
+<Highlight language={javascript} {code} let:highlighted>
+    <LineNumbers {highlighted} />
+</Highlight>
