212b817338c6fe76c1f26d68a88f650d7c16d0a0
Dr. Németh Tamás <tomuwhu@gmail.com>
2024-10-05 11:25:51 +0200

    .

--- src/routes/peldaprog/tologatos/+page.svelte
+++ src/routes/peldaprog/tologatos/+page.svelte
@@ -1,35 +1,27 @@
 <script>
-    import { onMount } from "svelte";
-    var n = 5;
-    const t = [...Array(n).keys()].map((y) =>
-        [...Array(n).keys()].map((x) => y * n + x + 1),
-    );
-    t[n - 1][n - 1] = "";
+    import { onMount } from "svelte"
+    var n = 5
+    const t = [...Array(n).keys()].map(y =>
+        [...Array(n).keys()].map((x) => y * n + x + 1))
+    t[n - 1][n - 1] = ""
     const check = (x, y) => {
         let v = [];
-        [
-            [1, 0],
-            [0, 1],
-            [-1, 0],
-            [0, -1],
-        ].forEach(([dx, dy]) => {
-            if (t[y + dy] && t[y + dy][x + dx] === "") v = [dx, dy];
-        });
-        return v;
+        [[1, 0], [0, 1], [-1, 0], [0, -1]].forEach(([dx, dy]) => {
+            if (t[y + dy] && t[y + dy][x + dx] === "") v = [dx, dy]
+        })
+        return v
     };
     const click = (x, y) => {
-        let [dx, dy] = check(x, y);
-        if (!dx && !dy) return;
-        t[y + dy][x + dx] = t[y][x];
-        t[y][x] = "";
+        let [dx, dy] = check(x, y)
+        if (!dx && !dy) return
+        t[y + dy][x + dx] = t[y][x]
+        t[y][x] = ""
     };
     onMount(() => {
-        for (let i = 0; i < n ** 5; i++) {
-            click(Math.floor(Math.random() * n), Math.floor(Math.random() * n));
-        }
-    });
+        for (let i = 0; i < n ** 5; i++) 
+            click(Math.floor(Math.random() * n), Math.floor(Math.random() * n))
+    })
 </script>
-
 <br /><br />
 <h1>Kirakós</h1>
 <div class="cont">
@@ -38,22 +30,17 @@
             <tr>
                 {#each row as cell, x}
                     <td
-                        class={(t[y - 1] && t[y - 1][x] === "") ||
-                        (t[y + 1] && t[y + 1][x] === "") ||
-                        (t[y] && t[y][x + 1] === "") ||
-                        (t[y] && t[y][x - 1] === "")
-                            ? "sz"
-                            : t[y] && t[y][x] === ""
-                              ? "w"
-                              : ""}
-                        on:click={() => click(x, y)}>{cell}</td
+                        class={
+                        (t[y - 1] && t[y - 1][x] === "") || (t[y + 1] && t[y + 1][x] === "") ||
+                        (t[y] && t[y][x + 1] === "") || (t[y] && t[y][x - 1] === "")
+                            ? "sz" : t[y] && t[y][x] === "" ? "w" : ""
+                        } on:click={() => click(x, y)}>{cell}</td
                     >
                 {/each}
             </tr>
         {/each}
     </table>
 </div>
-
 <style>
     table {
         margin: auto;
