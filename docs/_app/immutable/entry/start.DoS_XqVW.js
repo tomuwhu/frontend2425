a57405053ce0ee00ef73e5ee152e1f9ddb7746b4
Dr. Németh Tamás <tomuwhu@gmail.com>
2025-03-06 21:20:06 +0100

    .

---
+++ docs/_app/immutable/entry/start.DoS_XqVW.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.BjXKWvd_.js";export{t as start};
---
+++ src/routes/sveltetut/sveltekit_video/+page.svelte
@@ -0,0 +1,27 @@
+<br>
+<h1>SvelteKit - Drizzle - Lucia - MySQL - FullStack CRUD - Video tananyag</h1>
+<div class="ac">
+  <a target="_blank" href="https://www.youtube.com/watch?v=q4gjMCinRQY">Projekt és adatbázis létrehozása, Semantic UI, Style beállítása</a><br>
+  <a target="_blank" href="https://www.youtube.com/watch?v=J9b_T6YADsQ">Adat küldése szerver oldalról kliens oldalra</a><br>
+  <a target="_blank" href="https://www.youtube.com/watch?v=pfvvx_MRH0g">Projekt felépítése, Hooks</a><br>
+  <a target="_blank" href="https://www.youtube.com/watch?v=SzhkYpVvL9w">Hooks Információk kinyerése szerver oldalon</a><br>
+  <a target="_blank" href="https://www.youtube.com/watch?v=Rx4jCz5GHv4">Kijelentkezés űrlap módosítása</a><br>
+</div>
+<style>
+  div.ac {
+    display: block;
+    text-align: center;
+    margin: auto;
+    justify-content: center;
+  }
+  a {
+    all: unset;
+    display: inline-block;
+    padding: 2px;
+    margin: 3px;
+    cursor: pointer;
+  }
+  a:hover {
+    color: red;
+  }
+</style>
